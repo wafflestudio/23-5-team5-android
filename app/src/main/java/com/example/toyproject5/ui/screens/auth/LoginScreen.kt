41c130a41b1e1f966da117ea442ccd105593e032
sisihae <sihyunkim@snu.ac.kr>
2026-01-31 01:35:09 +0900

    fix: fix ci check fail

--- app/src/main/java/com/example/toyproject5/ui/screens/auth/LoginScreen.kt
+++ app/src/main/java/com/example/toyproject5/ui/screens/auth/LoginScreen.kt
@@ -1,7 +1,6 @@
 package com.example.toyproject5.ui.screens.auth
 
 import android.content.Context
-import android.util.Log
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
